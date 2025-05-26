e360fce3975d3846b6d890dce3772a7da9ea6a78
promahbubul <mahbublalam500@gmail.com>
2025-05-26 20:28:21 +0600

    Project Setup [TailwindCSS, React Router DOM, React Icons install and setup]

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
